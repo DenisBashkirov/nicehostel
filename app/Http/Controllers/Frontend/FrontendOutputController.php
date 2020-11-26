238a99a276fd999cba616b6f4320d437caadeb70
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-26 17:15:38 +0300

    Create FrontendOutputController.php

---
+++ app/Http/Controllers/Frontend/FrontendOutputController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Config;
+use App\Events\LeadFormSubmitted;
+use App\Feedback;
+use App\NavMenuItem;
+use App\Page;
+use App\PortfolioItem;
+use App\ServiceType;
+use App\TeamPerson;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\View;
+use Route;
+use Cookie;
+
+class FrontendOutputController extends FrontendBaseController
+{
+    protected $route_name;
+    protected $page;
+    protected $template;
+
+    public function __construct() {
+        $this->template = 'frontend.pages.';
+    }
+
+    public function home() {
+        $this->template .= 'home.home';
+
+        return $this->renderOutput();
+    }
+}
