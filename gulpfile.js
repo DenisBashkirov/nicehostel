16ea08708f2c87434f2b08d727f552424c208a3a
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-26 17:26:24 +0300

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('sass-framework', () => {
+    return gulp.src('./src/scss/framework/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('framework.css'))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass-project', () => {
+    return gulp.src('./src/scss/project/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('project.css'))
+        .pipe(gulp.dest('./css'));
+});
