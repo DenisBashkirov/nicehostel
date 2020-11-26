56a0607af4eba41385d138f19a37e82d6cd499d0
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-26 17:20:01 +0300

    Update main.blade.php

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 
-    @include('frontend.sources.styles.blade.php')
+    @include('frontend.sources.styles')
 
 </head>
 <body>
