444e57f7c49776326b34a51e4a2cf8a290bc7f7e
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-29 22:28:16 +0300

    Create features.blade.php

---
+++ resources/views/frontend/pages/home/features.blade.php
@@ -0,0 +1,11 @@
+<div class="features">
+    <div class="container">
+
+        <div class="features-list grid">
+
+            <object data="{{ asset('svg/features/comfort.svg') }}" type="image/svg+xml"></object>
+
+        </div>
+
+    </div>
+</div>
