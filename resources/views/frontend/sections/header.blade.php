a65f8dde07906309744dad6e9e095fbc35140906
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-29 19:45:55 +0300

    Create header.blade.php

---
+++ resources/views/frontend/sections/header.blade.php
@@ -0,0 +1,19 @@
+<header class="header text-white">
+    <div class="container">
+
+        <div class="header__logo">
+            <picture>
+                <img src="{{ asset('img/logo.svg') }}" alt="Nicehostel суть уютных хостелов логотип">
+            </picture>
+        </div>
+
+        <nav class="header__nav">
+
+        </nav>
+
+        <div class="header__phone">
+            <a class="no-underline" href="tel:8 (800) 505-16-57">8 (800) 505-16-57</a>
+        </div>
+
+    </div>
+</header>
