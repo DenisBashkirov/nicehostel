253d16bc423356a8a93bff6f4cb076bd4ccbb2e9
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-29 22:43:54 +0300

    Update item.blade.php

--- resources/views/components/frontend/components/features-list/item.blade.php
+++ resources/views/components/frontend/components/features-list/item.blade.php
@@ -1,4 +1,4 @@
 <div class="features-list__item text-center">
     <object class="xs-sm:h-80px md:h-96px lg:h-104px xl:h-132px" data="{{ asset('svg/features/' . $icon . '.svg') }}" type="image/svg+xml"></object>
-    <p class="xs-sm:mt-8px xs-sm:text-14 md-lg:text-18 xl:text-22 font-500 uppercase">{{ $text }}</p>
+    <p class="xs-sm:mt-8px xs-sm:text-14 md-lg:text-18 xl:text-22 font-500 uppercase">{!! $text !!}</p>
 </div>
