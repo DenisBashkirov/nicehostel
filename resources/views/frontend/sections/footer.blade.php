87a3f77a7b8d683ffdc16b2c5dd0dd837299ed76
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-30 00:43:18 +0300

    Update footer.blade.php

--- resources/views/frontend/sections/footer.blade.php
+++ resources/views/frontend/sections/footer.blade.php
@@ -3,5 +3,18 @@
 
         <h2 class="xs-sm:text-30 md-xl:text-48 font-700">Контакты</h2>
 
+        <div>
+
+            <div class="footer__phone">
+                <a class="no-underline" href="tel:+7 (800) 505-16-57">+7 (800) 505-16-57</a>
+            </div>
+
+            <dvi>
+                <p class="footer__address">г. Москва, ул. Арбат, дом 12, стр.1, офис 308</p>
+                <a class="no-underline" href="mailto:info@nicehostel.org">info@nicehostel.org</a>
+            </dvi>
+
+        </div>
+
     </div>
 </footer>
