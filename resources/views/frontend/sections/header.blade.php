e5ef8812b1172c14268faa5989d88765b5f4e63c
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-29 21:18:52 +0300

    Update header.blade.php

--- resources/views/frontend/sections/header.blade.php
+++ resources/views/frontend/sections/header.blade.php
@@ -1,17 +1,21 @@
 <header class="header absolute t-0 w-screen z-5 text-white">
-    <div class="container xs-sm:h-48 md-xl:h-64 flex items-center">
+    <div class="container flex xs-sm:justify-between xs-sm:h-48 md-xl:h-64 flex items-center">
 
-        <div class="header__logo">
-            <picture>
-                <img src="{{ asset('img/logo.svg') }}" alt="Nicehostel суть уютных хостелов логотип">
-            </picture>
+        <div>
+
+            <div class="header__logo">
+                <picture>
+                    <img class="xs-sm:h-40" src="{{ asset('img/logo/header.png') }}" alt="Nicehostel суть уютных хостелов логотип">
+                </picture>
+            </div>
+
+            <nav class="header__nav xs-sm:hidden">
+                <a class="no-underline text-white" href="#">Наши хостелы</a>
+                <a class="no-underline text-white" href="#">Франшиза</a>
+                <a class="no-underline text-white" href="#">Контакты</a>
+            </nav>
+
         </div>
-
-        <nav class="header__nav">
-            <a class="no-underline text-white" href="#">Наши хостелы</a>
-            <a class="no-underline text-white" href="#">Франшиза</a>
-            <a class="no-underline text-white" href="#">Контакты</a>
-        </nav>
 
         <div class="header__phone">
             <a class="no-underline text-white" href="tel:8 (800) 505-16-57">8 (800) 505-16-57</a>
