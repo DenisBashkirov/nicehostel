93e049afc27e938970b576689aaa82a5613247ec
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-29 19:07:29 +0300

    Update title-screen.blade.php

---
+++ resources/views/frontend/pages/home/title-screen.blade.php
@@ -0,0 +1,8 @@
+<div class="title-screen flex items-center xs-sm:h-468px text-white" style="background-image: url({{ asset('img/home/title-background.jpg') }})">
+    <div class="container">
+
+        <h1 class="xs-sm:text-40 font-900 uppercase">Nicehostel</h1>
+        <p class="xs-sm:text-30">Сеть уютных хостелов</p>
+
+    </div>
+</div>
