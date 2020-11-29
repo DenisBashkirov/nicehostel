4cac4ef2dc472a46568e61bfb1ee004d356bc621
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-29 21:55:27 +0300

    Update header.blade.php

--- resources/views/frontend/sections/header.blade.php
+++ resources/views/frontend/sections/header.blade.php
@@ -1,5 +1,5 @@
 <header class="header absolute t-0 w-screen z-5 text-white">
-    <div class="container flex xs-sm:justify-between xs-sm:h-48 md-xl:h-64 flex items-center">
+    <div class="container flex justify-between xs-sm:h-48 md-xl:h-64 flex items-center">
 
         <div class="md-xl:flex items-center">
 
@@ -9,7 +9,7 @@
                 </picture>
             </div>
 
-            <nav class="header__nav xs-sm:hidden md-lg:ml-42px xl:ml-80px">
+            <nav class="header__nav sx-32 xs-sm:hidden md-lg:ml-40px xl:ml-80px text-20">
                 <a class="no-underline text-white" href="#">Наши хостелы</a>
                 <a class="no-underline text-white" href="#">Франшиза</a>
                 <a class="no-underline text-white" href="#">Контакты</a>
@@ -18,7 +18,7 @@
         </div>
 
         <div class="header__phone xs-sm:hidden">
-            <a class="no-underline text-white" href="tel:8 (800) 505-16-57">8 (800) 505-16-57</a>
+            <a class="no-underline text-white text-24 font-700" href="tel:8 (800) 505-16-57">8 (800) 505-16-57</a>
         </div>
 
         <button class="hamburger md-xl:hidden" type="button">
