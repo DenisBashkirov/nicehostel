47db363d87d90e2ffed541e97742b3405ca80f63
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-29 22:37:52 +0300

    Update features.blade.php

--- resources/views/frontend/pages/home/features.blade.php
+++ resources/views/frontend/pages/home/features.blade.php
@@ -4,6 +4,9 @@
         <div class="features-list grid xs-sm:grid-cols-2 md-xl:grid-cols-4 gap">
 
             <x-frontend.components.features-list.item icon="comfort" text="Комфортные номера"/>
+            <x-frontend.components.features-list.item icon="wifi" text="Бесплатный wi-fi"/>
+            <x-frontend.components.features-list.item icon="safe" text="Персональный сейф"/>
+            <x-frontend.components.features-list.item icon="clean" text="Ежедневная уборка"/>
 
 
         </div>
