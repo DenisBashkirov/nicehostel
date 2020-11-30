3e0b8437db4613e6f1c874545fb8ba9f2c0aaff6
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-30 21:26:01 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', ['uses' => 'Frontend\FrontendOutputController@home', 'as' => 'home']);
+
+Route::get('/moscow/zemlyanoy-val-38', ['uses' => 'Frontend/FrontendOutputController@zemlyanoy_val', 'as' => 'zemlyanoy_val']);
