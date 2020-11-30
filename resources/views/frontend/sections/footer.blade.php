279746830b7004883b051a0db0c379c4ac06a525
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-11-30 20:07:19 +0300

    Update footer.blade.php

--- resources/views/frontend/sections/footer.blade.php
+++ resources/views/frontend/sections/footer.blade.php
@@ -1,17 +1,17 @@
-<footer class="footer bg-grey-0 xs-sm:pt-60 xs-sm:pb-40 md-xl:py-60">
+<footer class="footer bg-grey-0 xs-sm:pt-60 xs-sm:pb-40 md:pt-60 md:pb-40 lg-xl:py-60">
     <div class="container">
 
         <h2 class="xs-sm:text-30 md-xl:text-48 font-700">Контакты</h2>
 
-        <div class="xs-sm:mt-40">
+        <div class="xs-sm:mt-40 md:mt-60 lg-xl:mt-40">
 
             <div class="footer__phone">
                 <a class="flex items-center xs-sm:text-20 md-xl:text-24 xs-sm:sx-4 md-xl:sx-8 no-underline text-black" href="tel:+7(800)505-16-57"><i class="icon-mobile xs-sm:text-28 md-xl:text-32"></i><span>+7(800)505-16-57</span></a>
             </div>
 
-            <div class="xs-sm:mt-20">
+            <div class="lg-xl:flex lg:sx-60 xl:sx-120 xs-sm:mt-20 md-xl:mt-32">
                 <p class="flex items-center xs-sm:text-20 md-xl:text-24 xs-sm:sx-4 md-xl:sx-8"><i class="icon-pin xs-sm:text-28 md-xl:text-32"></i><span>г. Москва, ул. Арбат, дом 12, стр.1, офис 308</span></p>
-                <a class="flex items-center xs-sm:mt-20 xs-sm:text-20 md-xl:text-24 xs-sm:sx-4 md-xl:sx-8 no-underline text-black" href="mailto:info@nicehostel.org"><i class="icon-mail xs-sm:text-28 md-xl:text-32"></i><span>info@nicehostel.org</span></a>
+                <a class="flex items-center xs-sm:mt-20 md:mt-32 xs-sm:text-20 md-xl:text-24 xs-sm:sx-4 md-xl:sx-8 no-underline text-black" href="mailto:info@nicehostel.org"><i class="icon-mail xs-sm:text-28 md-xl:text-32"></i><span>info@nicehostel.org</span></a>
             </div>
 
         </div>
