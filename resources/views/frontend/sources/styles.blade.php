e7913f42fa0bf50029412e2f078ecafac8077a19
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-02 01:23:41 +0300

    Update styles.blade.php

--- resources/views/frontend/sources/styles.blade.php
+++ resources/views/frontend/sources/styles.blade.php
@@ -1,3 +1,7 @@
-<link rel="stylesheet" href="{{ asset('css/framework.css') }}">
-<link rel="stylesheet" href="{{ asset('css/frontend.css') }}">
+@if(env('APP_ENV') == 'production')
+    <link rel="stylesheet" href="{{ asset('css/main.min.css') }}">
+@else
+    <link rel="stylesheet" href="{{ asset('css/framework.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/frontend.css') }}">
+@endif
 <link rel="stylesheet" href="{{ asset('fonts/icons/style.css') }}">
