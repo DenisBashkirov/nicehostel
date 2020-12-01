2bac344a966e416c47eed06f920645151d558ae1
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-02 01:20:41 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('watch-styles', () => {
     )
 });
 
-gulp.task('build-css', () => {
+gulp.task('concat-css', () => {
     return gulp.src('./public/css/*.css')
         .pipe(concat('main.min.css'))
         .pipe(cleanCSS({level: 2}))
