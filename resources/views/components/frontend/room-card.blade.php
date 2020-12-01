a3ec8b98a5e9c2cb7265b82eac21597cdc5cc9ce
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-01 15:48:46 +0300

    commit

--- resources/views/components/frontend/room-card.blade.php
+++ resources/views/components/frontend/room-card.blade.php
@@ -1,7 +1,7 @@
 <div class="room-card flex-col mx-auto xs-sm:max-w-328 md-xl:p-20 b-radius-5">
 
     <picture class="w-full">
-        <img class="b-radius-3" src="{{ asset('img/samples/room.jpg') }}" alt="">
+        <img class="b-radius-3" src="{{ asset('img/hostels/' . $room->hostel->route . '/rooms/' . $room->slug. '.jpg') }}" alt="">
     </picture>
 
     <p class="flex-1 mt-20 text-18 lh-120">{{ $room->name }}</p>
