a5b4cea87f05977d753d2a2c8a3b9296dec9c005
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-01 19:57:44 +0300

    Update about.blade.php

--- resources/views/frontend/pages/hostel/about.blade.php
+++ resources/views/frontend/pages/hostel/about.blade.php
@@ -1,8 +1,8 @@
-<div class="container xl:py-100px">
+<div class="container xs-sm:py-40 xl:py-100px">
 
     <h2 class="xs-sm:text-30 md-xl:text-48 font-700">О хостеле</h2>
 
-    <div class="mt-40 text-20 sy-20">
+    <div class="xs-sm:mt-20 md-lg:mt-32 xl:mt-40 text-20 sy-20">
         {!! $hostel->about !!}
     </div>
 
