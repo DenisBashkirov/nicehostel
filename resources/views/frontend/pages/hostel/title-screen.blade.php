831cad04cf52f4ff416ed5aee6b83f80b238bfd2
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-01 20:36:05 +0300

    Update title-screen.blade.php

--- resources/views/frontend/pages/hostel/title-screen.blade.php
+++ resources/views/frontend/pages/hostel/title-screen.blade.php
@@ -6,7 +6,7 @@
     <div class="container md-lg:text-center">
 
         <h1 class="inline-block xs-sm:text-40 md-xl:text-64 pb-8 mb-8 font-900 uppercase b-bottom-3 b-style-solid" style="border-color: #fff">Nicehostel</h1>
-        <p class="xs-sm:text-24 md-xl:text-36">г. {{ $hostel->city . ' ' . $hostel->address }}</p>
+        <p class="xs-sm:text-24 md-xl:text-36">г. {{ $hostel->city . ', ' . $hostel->address }}</p>
 
         <div class="mt-40">
             <x-frontend.components.button.green color="green-0" font-size="xs-sm:text-18 md-xl:text-20" text="Номера и цены" link="#"/>
