97c4cee92714c9bad5ebaae0623f1e4b73c2337f
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-02 01:49:27 +0300

    Update green.blade.php

--- resources/views/components/frontend/components/button/green.blade.php
+++ resources/views/components/frontend/components/button/green.blade.php
@@ -1,1 +1,1 @@
-<a class="hostels-list-grid__item-link button button_color-{{ $color }} hover_bg-green-20 no-underline {{ $fontSize }} font-500 b-radius-5" href="{{ $link }}">{{ $text }}</a>
+<a class="hostels-list-grid__item-link button button_color-green-0 hover_bg-green-20 no-underline {{ $fontSize }} font-500 b-radius-5" href="{{ $link }}">{{ $text }}</a>
