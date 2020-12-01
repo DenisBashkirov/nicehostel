395adfc99e68deb6c7265df83ab27228ca71bdea
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-01 20:55:40 +0300

    Update footer.blade.php

--- resources/views/frontend/sections/footer.blade.php
+++ resources/views/frontend/sections/footer.blade.php
@@ -6,12 +6,12 @@
         <div class="xs-sm:mt-40 md:mt-60 lg-xl:mt-40">
 
             <div class="footer__phone">
-                <a class="flex items-center xs-sm:text-20 md-xl:text-24 xs-sm:sx-4 md-xl:sx-8 no-underline text-black" href="tel:+7(800)505-16-57"><i class="icon-mobile xs-sm:text-28 md-xl:text-32"></i><span>{{ $phone }}</span></a>
+                <a class="flex items-center xs-sm:text-20 md-xl:text-24 xs-sm:sx-4 md-xl:sx-8 no-underline text-black hover_color-grey-90" href="tel:+7(800)505-16-57"><i class="icon-mobile xs-sm:text-28 md-xl:text-32"></i><span>{{ $phone }}</span></a>
             </div>
 
             <div class="lg-xl:flex lg:sx-60 xl:sx-120 xs-sm:mt-20 md-xl:mt-32">
                 <p class="flex items-center xs-sm:text-20 md-xl:text-24 xs-sm:sx-4 md-xl:sx-8"><i class="icon-pin xs-sm:text-28 md-xl:text-32"></i><span>{{ $address }}</span></p>
-                <a class="flex items-center xs-sm:mt-20 md:mt-32 xs-sm:text-20 md-xl:text-24 xs-sm:sx-4 md-xl:sx-8 no-underline text-black" href="mailto:info@nicehostel.org"><i class="icon-mail xs-sm:text-28 md-xl:text-32"></i><span>{{ $email }}</span></a>
+                <a class="flex items-center xs-sm:mt-20 md:mt-32 xs-sm:text-20 md-xl:text-24 xs-sm:sx-4 md-xl:sx-8 no-underline text-black hover_color-grey-90" href="mailto:info@nicehostel.org"><i class="icon-mail xs-sm:text-28 md-xl:text-32"></i><span>{{ $email }}</span></a>
             </div>
 
         </div>
