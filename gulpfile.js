2d0f83d1a781fb75300310ec33c7c9d1e7310afd
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-02 22:21:20 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -64,3 +64,17 @@ gulp.task('webp', () =>
         .pipe(webp())
         .pipe(gulp.dest('./public/img/'))
 );
+
+gulp.task('critical-css', function () {
+    return gulp.src('./public/css/main.min.css')
+        .pipe(criticalCss({
+            out: 'critical.min.css',
+            url: 'http://nicehostel',
+            width: 1800,
+            height: 900,
+            strict: true,
+            userAgent: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+        }))
+        .pipe(cleanCSS({level: 2}))
+        .pipe(gulp.dest('./public/css/'));
+});
