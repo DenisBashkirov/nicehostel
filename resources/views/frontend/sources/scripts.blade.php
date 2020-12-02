59fdc03b26248ca6eef808731432f0a74c186069
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-03 01:11:30 +0300

    Update scripts.blade.php

--- resources/views/frontend/sources/scripts.blade.php
+++ resources/views/frontend/sources/scripts.blade.php
@@ -1,6 +1,7 @@
-<script
+<script defer
     src="https://code.jquery.com/jquery-3.5.1.min.js"
     integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-    crossorigin="anonymous"></script>
-<script src="{{ asset('libs/jquery.easing/jquery.easing.1.3.min.js') }}"></script>
-<script src="{{ asset('js/main.js') }}"></script>
+    crossorigin="anonymous"
+    importance="low"></script>
+<script defer src="{{ asset('libs/jquery.easing/jquery.easing.1.3.min.js') }}" importance="low"></script>
+<script defer src="{{ asset('js/main.js') }}"></script>
