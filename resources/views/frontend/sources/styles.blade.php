5825a7397d67451929f6a426fbc2d727d8a6b95d
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-02 23:02:42 +0300

    Update styles.blade.php

--- resources/views/frontend/sources/styles.blade.php
+++ resources/views/frontend/sources/styles.blade.php
@@ -1,6 +1,9 @@
 @if(env('APP_ENV') == 'production')
     <link rel="preload" href="{{ asset('css/critical.min.css') }}" as="style" onload="this.rel='stylesheet'">
     <link rel="preload" href="{{ asset('css/main.min.css') }}" as="style" onload="this.rel='stylesheet'">
+    <link rel="preload" href="{{ asset('fonts/Roboto/Roboto-Bold.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{ asset('fonts/Roboto/Roboto-Regular.woff2') }}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{ asset('fonts/Roboto/Roboto-Medium.woff2') }}" as="font" type="font/woff2" crossorigin>
 @else
     <link rel="stylesheet" href="{{ asset('css/framework.css') }}">
     <link rel="stylesheet" href="{{ asset('css/frontend.css') }}">
