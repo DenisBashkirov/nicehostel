ac734ed432a37055dc176d252648ef3655155f5e
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-02 22:57:40 +0300

    Update styles.blade.php

--- resources/views/frontend/sources/styles.blade.php
+++ resources/views/frontend/sources/styles.blade.php
@@ -1,5 +1,6 @@
 @if(env('APP_ENV') == 'production')
-    <link rel="stylesheet" href="{{ asset('css/main.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/critical.min.css') }}">
+    <link rel="preload" href="{{ asset('css/main.min.css') }}" as="style" onload="this.rel='stylesheet'">
 @else
     <link rel="stylesheet" href="{{ asset('css/framework.css') }}">
     <link rel="stylesheet" href="{{ asset('css/frontend.css') }}">
