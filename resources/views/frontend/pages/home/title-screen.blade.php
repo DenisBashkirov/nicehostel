eba8b56b3f17561aeadd7e57ed3ed970309ec3e1
DenisBashkirov <bashkirov.denis@era-digital.ru>
2020-12-03 01:07:54 +0300

    Update title-screen.blade.php

--- resources/views/frontend/pages/home/title-screen.blade.php
+++ resources/views/frontend/pages/home/title-screen.blade.php
@@ -12,7 +12,7 @@
         <source data-srcset="{{ asset('img/home/title-background_xs@2.webp') }}" media="(-webkit-min-device-pixel-ratio: 2)" type="image/webp">
         <source data-srcset="{{ asset('img/home/title-background_xs.webp') }}" media="(-webkit-device-pixel-ratio: 1)" type="image/webp">
         <source data-srcset="{{ asset('img/home/title-background_xs@2.jpg') }}" media="(-webkit-min-device-pixel-ratio: 2)">
-        <img class="overlay w-full h-full object-cover" src="{{ asset('img/home/title-background_xs.jpg') }}" alt="Nicehostel">
+        <img class="overlay w-full h-full object-cover" src="{{ asset('img/home/title-background_xs.jpg') }}" importance="high" alt="Nicehostel">
 
     </picture>
     <div class="container md-lg:text-center">
