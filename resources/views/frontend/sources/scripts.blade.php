541694ef4b2e8314407897412b9810f508f2c875
DenisBashkirov <bashkirov.denis@era-digital.ru>
2021-04-27 12:31:42 +0300

    Update scripts.blade.php

--- resources/views/frontend/sources/scripts.blade.php
+++ resources/views/frontend/sources/scripts.blade.php
@@ -4,3 +4,7 @@
     crossorigin="anonymous"></script>
 <script src="{{ asset('libs/jquery.easing/jquery.easing.1.3.min.js') }}"></script>
 <script src="{{ asset('js/main.js') }}"></script>
+
+@if(env('APP_ENV') == 'production')
+    <script type="text/javascript" src="//api.venyoo.ru/wnew.js?wc=venyoo/default/science&widget_id=6755342139790973"></script>
+@endif
